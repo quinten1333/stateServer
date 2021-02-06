f774548c61c75bea099d757a13652cd365ba9a4a
Quinten Coltof <quintencoltof1@gmail.com>
2021-02-06 20:57:22 +0100

    Added debug msg script.

---
+++ msg-debug.js
@@ -0,0 +1,27 @@
+const net = require('net');
+const readline = require("readline");
+const conf = require('./config').server;
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const question = () => new Promise((resolve) => rl.question('', resolve))
+
+const client = new net.Socket();
+client.connect(conf.listen, async () => {
+    console.log('Connected');
+
+    while (true) {
+        client.write(await question());
+    }
+});
+
+client.on('data', function(data) {
+	console.log('Received: ' + data);
+});
+
+client.on('close', function() {
+	console.log('Connection closed');
+});
